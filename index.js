439738c878dde990a9d487bffcd090ae67425c0e
Lalit Bagga <l.bagga@combyne.ag>
2026-06-17 11:51:39 -0400

    chore: add prom-client dependency and implement metrics endpoint

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const client = require('prom-client')
 const app = express()
 const port = 3000
+
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics()
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Three Tier App' })
@@ -8,6 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' })
+})
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.end(await client.register.metrics())
 })
 
 app.listen(port, () => {
